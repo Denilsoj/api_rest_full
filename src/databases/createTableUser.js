92403a9eda62f89606db9450f4f0d223a5fb41d0
Denilson Oliveira <denilso-oliveira@pop-os.localdomain>
2024-03-01 16:01:45 -0300

    feat/add-user-controller-and-user-routes

--- src/databases/createTableUser.js
+++ src/databases/createTableUser.js
@@ -1,7 +1,14 @@
 const { pool } = require('./poolDb');
 
 pool.getConnection((error, conn) => {
-  const sql = 'CREATE TABLE user (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,name VARCHAR(255) NOT NULL, password_hash VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL)';
+  const sql = `
+  CREATE TABLE user (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL, 
+  email VARCHAR(255) NOT NULL UNIQUE,
+  CHECK (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
+  )`;
   conn.query(sql, (err, ress) => {
     if (err) throw err;
 
--- src/databases/poolDb.js
+++ src/databases/poolDb.js
@@ -5,13 +5,13 @@ const pool = mysql2.createPool({
   user: 'root',
   database: 'mydb',
   password: 'senha',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 10, // Máximo de conexões inativas; o valor padrão é o mesmo que "connectionLimit"
-//   idleTimeout: 60000, // Tempo limite das conexões inativas em milissegundos; o valor padrão é "60000"
-//   queueLimit: 0,
-//   enableKeepAlive: true,
-//   keepAliveInitialDelay: 0,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // Máximo de conexões inativas; o valor padrão é o mesmo que "connectionLimit"
+  idleTimeout: 60000, // Tempo limite das conexões inativas em milissegundos; o valor padrão é "60000"
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
 module.exports = {
